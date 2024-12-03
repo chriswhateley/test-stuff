0a7d5eaf55d1956c3abf6d424ec473c5b1fcbf9c
Chris Whateley <881698+chriswhateley@users.noreply.github.com>
2024-12-03 13:00:50 -0800

    Update App.java (#1284)

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,6 +6,8 @@ package com.mycompany.app;
  */
 public class App 
 {
+    String PASSWORD = 1234;
+    
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
